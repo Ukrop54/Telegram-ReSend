1b7f72374f504c5655f050716b0dd1fdfcc3f455
Ukrop54 <yaroslavkhor55@gmail.com>
2026-02-21 18:35:23 +0100

    supabase test install

--- netlify/functions/signup.js
+++ netlify/functions/signup.js
@@ -1,3 +1,8 @@
+// import { createClient } from "@supabase/supabase-js";
+// import { Database } from "./database.types";
+
+// const supabase = createClient < Database > (process.env.SUPABASE_DATABASE_URL, process.env.SUPABASE_ANON_KEY);
+
 import { createClient } from "@supabase/supabase-js";
 
 export async function handler(event) {
